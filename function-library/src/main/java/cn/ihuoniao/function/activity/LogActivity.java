b4fdc2c8d001ba0c1bda16cc25fb6ae31b10bb31
sunhaoyang <20977372@qq.com>
2017-03-19 18:13:00 +0800

    add

--- function-library/src/main/java/cn/ihuoniao/function/activity/LogActivity.java
+++ function-library/src/main/java/cn/ihuoniao/function/activity/LogActivity.java
@@ -3,6 +3,8 @@ package cn.ihuoniao.function.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import cn.ihuoniao.function.R;
@@ -12,9 +14,10 @@ import cn.ihuoniao.function.util.LogCatUtil;
  * Created by apple on 2017/3/19.
  */
 
-public class LogActivity extends AppCompatActivity {
+public class LogActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tvLog = null;
+    private Button btnSave = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,7 +25,16 @@ public class LogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log);
 
         tvLog = (TextView) findViewById(R.id.tv_log);
+        btnSave = (Button) findViewById(R.id.btnSave);
 
         tvLog.setText(LogCatUtil.getLog());
+        btnSave.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnSave) {
+
+        }
     }
 }
